4dbafc7656ae5e83e249f67f7a4e8d6f5ea45564
Inna Kuklina <in.d2010@yandex.ru>
2025-02-16 19:42:43 +0300

    fixed mistakes

--- src/main/java/org/example/SpringCloudClient.java
+++ src/main/java/org/example/SpringCloudClient.java
@@ -1,11 +1,32 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
-public class SpringCloudClient {
+public class SpringCloudClient implements CommandLineRunner {
+
+    @Value("${user.role}")
+    private String role;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudClient.class, args);
+    }
+
+    @GetMapping(
+            value = "/about/{username}",
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public String about(@PathVariable("username") String username) {
+        return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, role);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
     }
 }
